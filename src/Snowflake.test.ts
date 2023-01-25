c7e96b278eadbbab6978ffc2931e287b1b281128
Nathan Faucett <nathanfaucett@gmail.com>
2023-01-25 07:31:47 -0500

    update tests

--- src/Snowflake.test.ts
+++ src/Snowflake.test.ts
@@ -3,13 +3,17 @@ import { Snowflake } from "./Snowflake";
 
 tape("defaults", (assert) => {
   const snowflake = new Snowflake({
-    instanceId: 0n,
+    instanceId: 2047n,
     customEpoch: 1674400749400n,
   });
-  assert.equals(snowflake.getInstanceId(), 0n);
+  assert.equals(snowflake.getInstanceId(), 2047n);
   assert.equals(snowflake.getCustomEpoch(), 1674400749400n);
-  assert.equals(snowflake.idFromTimestamp(1674400778395n), 121613844480n);
-  assert.deepEqual(snowflake.parseId(121613844480n), [1674400778395n, 0n, 0n]);
+  assert.equals(snowflake.idFromTimestamp(1674400778395n), 121615940608n);
+  assert.deepEqual(snowflake.parseId(121615940608n), [
+    1674400778395n,
+    2047n,
+    0n,
+  ]);
   assert.end();
 });
 
@@ -19,18 +23,18 @@ tape("128bit", (assert) => {
     epochBits: 64n,
     instanceIdBits: 48n,
     sequenceBits: 16n,
-    instanceId: 0n,
+    instanceId: 32767n,
     customEpoch: 1674400749400n,
   });
-  assert.equals(snowflake.getInstanceId(), 0n);
+  assert.equals(snowflake.getInstanceId(), 32767n);
   assert.equals(snowflake.getCustomEpoch(), 1674400749400n);
   assert.equals(
     snowflake.idFromTimestamp(1674400778395n),
-    534863344417208449105920n
+    534863344417210596524032n
   );
-  assert.deepEqual(snowflake.parseId(534863344417208449105920n), [
+  assert.deepEqual(snowflake.parseId(534863344417210596524032n), [
     1674400778395n,
-    0n,
+    32767n,
     0n,
   ]);
   assert.end();
@@ -38,7 +42,7 @@ tape("128bit", (assert) => {
 
 tape("should be able to generate thousands of unique ids", (assert) => {
   const ids = new Set<bigint>();
-  for (const id of new Snowflake().iter()) {
+  for (const id of new Snowflake()) {
     ids.add(id);
     if (ids.size >= 1024000) {
       break;
